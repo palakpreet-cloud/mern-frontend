00b2741ccfd22a3b8af94b66698a1612a005c5af
Palakpreet Kaur <hs960970@gmail.com>
2026-09-05 11:54:23 +0530

    Update BASEURL to new server address

--- src/components/services/ApiService.js
+++ src/components/services/ApiService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 // const BASEURL="http://localhost:5000"
-const BASEURL="http://localhost:5001"
+const BASEURL="http://65.1.93.114:5001"
 class ApiServices{
     getToken(){
         let token=sessionStorage.getItem("token")
